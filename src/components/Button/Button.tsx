061b75cf7680e6d874ac7c42c2aa7d75b746c5ee
Dirosaki <diegosgomes27@gmail.com>
2022-10-13 01:49:11 -0300

    feat: Homepage

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,25 @@
-import { ReactNode } from 'react'
+import { ButtonHTMLAttributes, ReactNode } from 'react'
 import { Slot } from '@radix-ui/react-slot'
 
-export interface ButtonProps {
+import clsx from 'clsx'
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	children: ReactNode
 	asChild?: boolean
+	className?: string
 }
 
-export function Button({ asChild, children }: ButtonProps) {
+export function Button({ asChild, children, className, ...props }: ButtonProps) {
 	const Component = asChild ? Slot : 'button'
 
 	return (
-		<Component className='py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white'>
+		<Component
+			className={clsx(
+				'py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
+				className
+			)}
+			{...props}
+		>
 			{children}
 		</Component>
 	)
